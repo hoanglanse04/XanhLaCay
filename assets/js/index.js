af0b6f5e567de6be0f451194c8d0cc7d6db200ee
hungka2013 <hoanglando16@gmail.com>
2024-05-29 22:26:57 +0700

    feat: add home page

---
+++ assets/js/index.js
@@ -0,0 +1,169 @@
+window.onscroll = function() {
+  var navbar = document.getElementById('navbar');
+  let navbar_item = document.querySelectorAll('.navbar-nav a')
+
+  console.log(navbar_item)
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled');
+    navbar_item.forEach(item =>{
+      item.style.color ='black';
+    })
+  } else {
+    navbar.classList.remove('scrolled');
+    navbar_item.forEach(item =>{
+      item.style.color ='#96dd96';
+    })
+  }
+};
+
+
+// 
+const listItem =[
+  {
+    "id": 1,
+    "name": "Bàng Sing",
+    "price": 200.000,
+    "thumbnail": "https://tuongcaygia.vn/wp-content/uploads/2020/10/vuoong-2-1.jpg",
+    "desc": " Bao gồm: Cây Bàng Sing, chậu sứ dáng hạt dẻ.Bàng Sing là loại cây cảnh khá độc đáo thích hợp trồng trang trí trong nhà, cơ quan, văn phòng làm việc. Bạn cũng có thể trồng loại mini để bàn, kệ sách và trồng loại lớn để đặt góc phòng.",
+    "care": "Bàng Sing có thể chịu bóng râm, sống được dưới ánh đèn huỳnh quang. Tuy nhiên, để cây sinh trưởng và phát triển tốt, cần trồng cây ở nơi có ánh sáng khuếch tán hoặc đem cây phơi nắng nhẹ khoảng 2-3 lần/tuần, mỗi lần khoảng 3 tiếng. Tránh để dưới ánh nắng mạnh, bởi có thể làm lá cây phai màu hoặchéo vàng Cây không cần quá nhiều nước, nếu để ngoài trời tuần tưới 1 lần. Cây trồng chậu trong nhà nên tưới lượng nước vừa phải tuần tưới khoảng 2 lần.",
+    "discount":"10"
+    
+  },
+  {
+    "id": 2,
+    "name": "Cẩm nhung",
+    "price": "90.000",
+    "thumbnail": "https://cdn.eva.vn/upload/3-2022/images/2022-07-01/image3-1656638489-800-width640height480.jpg",
+    "desc": " Bao gồm: Cây Cẩm nhung hoặc kèm chậu. Cẩm nhung là cây cảnh nhiều màu sắc (đỏ, hồng, trắng, xanh) dễ chăm sóc được nhiều người ưa chuộng.Ngoài ra cây mang ý nghĩa may mắn, suôn sẻ cho người trồng. Vậy nên nó còn có tên gọi khác là “lá may mắn”.",
+    "care": "Cây phù hợp với ánh sáng gián tiếp hoặc nắng nhẹ nên có thể sống trong nhà hay văn phòng. Tuy nhiên, nếu thiếu sáng lâu, lá của nó sẽ bị mất màu và thân sẽ dài ra. Cẩm nhung là loài cây ưa ẩm nên cần tưới cây hàng ngày (vào buổi sáng), có thể kết hợp tưới phun sương vào lá giúp làm sạch và tốt cho quá trình quang hợp.",
+    "discount":"20"
+    
+  },
+  {
+    "id": 3,
+    "name": "Cau tiểu trâm",
+    "price": "100.000",
+    "thumbnail": "https://static1.cafeland.vn/cafelandnew/hinh-anh/2020/06/27/144/image-20200627090122-1.jpeg",
+    "desc": " Bao gồm: Cây Cau tiểu trâm, chậu sứ vành miệng hồng. Cau tiểu trâm là cây cảnh phong thủy hợp mệnh mộc mang đến nhiều tài lộc cũng như sự may mắn. Cây cau tiểu trâm thường được dùng để trang trí văn phòng, bàn làm việc khá là đẹp.",
+    "care": "Cây ưa sáng, nhưng vẫn sống được trong điều kiện ánh sáng yếu. Cây để trong nhà, tuần tưới 1-2 lần khi thấy đất khô. Thỉnh thoảng cho cây phơi nắng nhẹ tháng 1-2 buổi để cây quang hợp tốt hơn.",
+    "discount":"15"
+  },
+  {
+    "id": 4,
+    "name": "Đa cẩm thạch",
+    "price": "270.000",
+    "thumbnail": "https://vuacayxanh.net/wp-content/uploads/2024/02/cay-da-cam-thach-1.jpeg",
+    "desc": " Bao gồm: Cây Đa cẩm thạch, chậu xi măng.Đa cẩm thạch mang ý nghĩa trường thọ, bền bỉ phù hợp làm quà tặng cho người lớn tuổi. Màu sắc lá đỏ bắt mắt, trang trí nhà cửa sẽ tươi mát hơn.",
+    "care": "Đa cẩm thạch thích hợp sống nơi ánh sáng gián tiếp. Lý tưởng cho cây là vị trí cạnh cửa sổ, tránh ánh nắng trực tiếpƯa đất khô thoáng, tuần tưới 2-3 lần khi đất khô.Thường xuyên tưới phun sương lên bề mặt lá để cung cấp độ ẩm cho cây",
+    "discount":"18",
+  },
+  {
+    "id": 5,
+    "name": "Hạnh phúc ",
+    "price": "280.000",
+    "thumbnail": "https://cdn.tgdd.vn/Files/2021/08/02/1372490/cach-trong-va-cham-soc-cay-hanh-phuc-don-gian-cho-dang-dep-202108021456341533.jpg",
+    "desc": " Bao gồm: Cây Hạnh phúc, chậu sứ trụ trắng.Hạnh phúc có ý nghĩa gắn kết các mối quan hệ, giúp các thành viên trong gia đình đoàn kết, hòa thuận, giữ gìn hòa khí.Trồng cây Hạnh phúc trong nhà còn giúp mang đến sự sung túc cho gia chủ, thể hiện sự tinh tế trong tính cách.Một chậu Hạnh phúc làm quà tặng trong dịp cưới hỏi cũng là một lời chúc đầy ý nghĩa cho các cặp đôi.",
+    "care": "Là loài cây ưa ẩm cần tưới thường xuyên duy trì độ ẩm cho đất. Nếu trồng ngoài trời, bạn nên tưới cây mỗi ngày 1 lần vào sáng sớm hoặc chiều tối. Nếu trồng trong văn phòng, ít tiếp xúc ánh nắng thì có thể tưới 3 lần mỗi tuần. Bạn cũng nên quan sát đất để điều chỉnh lượng nước tưới cho phù hợp, không để cây bị ngập úng.Trồng cây ở các vị trí nhiều ánh sáng nhưng không tiếp xúc trực tiếp với ánh nắng mặt trời. Đồng thời mỗi tuần bạn nên cho cây phơi nắng sớm khoảng 1 tiếng để kích thích cây quang hợp.",
+    "discount":"20"
+  },
+  {
+    "id": 6,
+    "name": "Lá măng ",
+    "price": "200.000",
+    "thumbnail": "https://my-test-11.slatic.net/p/ab761a95d0b018c97b6b70efa4609910.jpg",
+    "desc": "Bao gồm: Cây Lá măng, chậu đất nung trứng da rắn.Lá măng hay còn gọi là Thủy tùng tượng trưng cho sự thanh khiết, ngay thẳng không nhún nhường nên hợp với gia chủ là nam.Nếu trồng trong nhà cây thủy tùng thì phúc lộc kim tiền của gia chủ lại càng dồi dào và thịnh vượng. Thích hợp làm quà tặng.",
+    "care": "Thủy tùng là cây ưa sáng nên thích hợp được đặt để trên bàn làm việc, góc cửa sổ nơi có nhiều ánh sáng, tránh ánh sáng trực tiếp.Cây không háo nước, tưới 2-3 lần 1 tuần.Khi cây xuất hiện các lá vàng, lá úa, lá héo thì phải cắt bỏ đi ngay tránh ảnh hưởng đến sự phát triển của những lá khác.",
+    "discount":"13"
+  },
+  {
+    "id":7,
+    "name": "Lan hạt dưa",
+    "price": "100.000",
+    "thumbnail":"https://gionghoadep.com/wp-content/uploads/2023/11/dac-diem-cua-cay-lan-hat-dua.jpg",
+    "desc":"Cây hạt dưa là dòng cây thân leo phù hợp để trồng trong chậu treo trang trí tạo không gian tươi mát.Ngoài ra cây mang ý nghĩa tài lộc, may mắn và sự kiên trì bền bỉ nên sẽ là một món quà tặng tuyệt vời.",
+    "care":"Cây Hạt Dưa dễ thích nghi với hầu hết các môi trường, kể cả nơi bóng râm hoàn toàn hoặc chỉ có ánh sáng nhẹ.  Nên tưới nước làm mát hằng ngày. Tuy nhiên mỗi lần không nên tưới quá nhiều nước để tránh thân lá bị ngập úng.Thỉnh thoảng cho cây phơi nắng nhẹ tháng 1-2 buổi để cây quang hợp tốt hơn.",
+    "discount":"15"
+  },
+  {
+    "id":8,
+    "name": "Lưỡi hổ",
+    "price": "120.000",
+    "thumbnail":"https://cdn.tgdd.vn/Files/2021/01/07/1318539/cay-luoi-ho-la-gi-y-nghia-tac-dung-va-nhung-luu-y-khi-trong-cay-luoi-ho-202112291713060793.jpg",
+    "desc":"Lưỡi hổ có tác dụng thanh lọc không khí, hấp thụ chất ô nhiễm, cải thiện không gian sống.Được nhiều người ưa chuộng để trang trí sân vườn, nhà cửa, văn phòng.",
+    "care":"Cây trồng trong nhà chỉ cần tưới 1-2 lần/ tuần.Cho ra nắng khoảng 6 tiếng/ tuần vào buổi sáng. Lưỡi hổ có khả năng chịu hạn tốt nên trồng sân vườn không phải chăm sóc nhiều.",
+    "discount":"12"
+
+  },
+  {
+    "id":9,
+    "name": "Thường xuân ",
+    "price": "100.000",
+    "thumbnail":"https://cayyeuthuong.com/uploads/noidung/cay-thuong-xuan-0-754.jpg",
+    "desc":"Cây có tác dụng thanh lọc không khí, giảm bụi bẩn trong môi trường xung quanh.Thường xuân tượng trưng cho tình hữu nghĩ, tuổi trẻ và sự vĩnh hằng.",
+    "care":"Thường xuân có thể phát triển dưới ánh nắng trực tiếp hoặc trong bóng râm, tránh nắng gắt.",
+    "discount":"10"
+  }
+]
+
+const reListItem = listItem.slice().reverse();
+
+let listProduct = document.getElementById('list-product');
+listProduct.innerHTML='';
+listItem.forEach(item =>{
+  listProduct.innerHTML +=`
+  <div class="product-item text-center">
+    <div class="product-img">
+    <img src=${item.thumbnail}>
+    </div>
+    <div class="product-content">
+    <h5>${item.name}</h6>
+    <h6>${item.price}đ</h5>
+    <p class ="product-desc">${item.desc}</p>
+    <p class ="product-guide">${item.care}</p>
+    
+    <button><i class="bi bi-bag-fill"></i><span>Mua ngay</span></button>
+    
+    </div>
+  </div>
+  `
+  
+})
+
+let listProductDiscount = document.getElementById('list-product-discount');
+listProductDiscount.innerHTML='';
+reListItem.forEach(item =>{
+  listProductDiscount.innerHTML +=`
+  <div class="product-item text-center">
+    <div class ="discount">${item.discount}%</div>
+    <div class="product-img">
+    <img src=${item.thumbnail}>
+    </div>
+    <div class="product-content">
+    <h5>${item.name}</h6>
+    <h6>${item.price}đ</h5>
+    <p class ="product-desc">${item.desc}</p>
+    <p class ="product-guide">${item.care}</p>
+    
+    <button><i class="bi bi-bag-fill"></i><span>Mua ngay</span></button>
+    
+    </div>
+  </div>
+  `
+  
+})
+
+  const list_product = document.querySelector('#list-product');
+
+  list_product.addEventListener('wheel', function(event) {
+      if (event.deltaY !== 0) {
+          event.preventDefault();
+          list_product.scrollLeft += event.deltaY;
+      }
+  });
+  const list_product_discount = document.querySelector('#list-product-discount');
+  list_product_discount.addEventListener('wheel', function(event) {
+    if (event.deltaY !== 0) {
+        event.preventDefault();
+        list_product_discount.scrollLeft += event.deltaY;
+    }
+});
